4b9c7d3c9b95343d2fc0adb47f51a5607e0c78d9
Ed Kurilla <ekurilla@me.com>
2012-01-29 17:29:09 -0500

    Make a basic User model (including secure passwords)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
+    ENV["RAILS_ENV"] ||= 'test'
     require File.expand_path("../../config/environment", __FILE__)
     require 'rspec/rails'
     require 'rspec/autorun'
